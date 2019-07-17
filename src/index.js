96f53fabe23266be437beefc777f39c6b029bcc9
Sala 310 <sala310@poliacademico.edu.loc>
2019-07-17 12:30:47 -0500

    actualizado

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './componets/About';
+
+ReactDOM.render(
+    <About /> ,  
+    document.getElementById("app")
+);  
